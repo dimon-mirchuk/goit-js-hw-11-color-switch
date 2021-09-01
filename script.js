e73a24e338adc06d978d9917344c2eee259e9696
dimon-mirchuk <dimon.mirchuk@gmail.com>
2021-09-01 11:27:01 +0300

    Create color switcher

---
+++ script.js
@@ -0,0 +1,36 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const bodyRef = document.querySelector("body");
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let changeId = null;
+
+startBtnRef.addEventListener("click", startChangeColor);
+stopBtnRef.addEventListener("click", stopChangeColor);
+
+function startChangeColor() {
+  startBtnRef.disabled = true;
+
+  changeId = setInterval(() => {
+    const colorIndex = randomIntegerFromInterval(0, 5);
+    bodyRef.style.backgroundColor = colors[colorIndex];
+    console.log(colorIndex);
+  }, 1000);
+}
+
+function stopChangeColor() {
+  startBtnRef.disabled = false;
+  clearInterval(changeId);
+}
